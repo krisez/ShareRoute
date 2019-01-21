3bc08b822843800802ccf8089ac7b027ec2aa68b
krisez <740879437@qq.com>
2019-01-21 19:45:49 +0800

    修改依赖

--- app/src/main/java/cn/krisez/shareroute/APP.java
+++ app/src/main/java/cn/krisez/shareroute/APP.java
@@ -3,7 +3,10 @@ package cn.krisez.shareroute;
 import android.app.Application;
 import android.content.Context;
 
+import cn.krisez.kotlin.net.API;
 import cn.krisez.network.NetWorkUtils;
+import cn.krisez.network.bean.Result;
+import cn.krisez.network.handler.ResultHandler;
 
 
 public class APP extends Application {
@@ -15,6 +18,17 @@ public class APP extends Application {
         super.onCreate();
         sContext = getApplicationContext();
         NetWorkUtils.INSTANCE().url("http://192.168.137.1:8080/");
+        NetWorkUtils.INSTANCE().create(new NetWorkUtils.NetApi().api(API.class).login("")).handler(new ResultHandler() {
+            @Override
+            public void onSuccess(Result result) {
+
+            }
+
+            @Override
+            public void onFailed(String msg) {
+
+            }
+        });
     }
 
     public static Context getContext(){
