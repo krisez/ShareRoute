26c427c285a2f49c0a44ea1b2c45e035466dec75
krisez <740879437@qq.com>
2019-03-18 09:54:19 +0800

    用户面板UI完成

---
+++ app/src/test/java/cn/krisez/shareroute/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package cn.krisez.shareroute;
+
+import com.google.gson.Gson;
+
+import org.junit.Test;
+
+import cn.krisez.kotlin.aa;
+import cn.krisez.network.bean.Result;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+
+    }
+}
